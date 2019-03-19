ccf93674d4fdd6988eacc0ece0322529d85e84a4
mukasine marie claire <mclaremukasine@gmail.com>
2019-03-19 11:32:45 +0200

    adding class fonctoin search-by-category in models.py

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -47,15 +47,12 @@ class Image(models.Model):
         return images
 
     @classmethod
-    def search_by_category(cls,search_term):
-        images = cls.objects.filter(categories__name__contains = search_term)
-        if len(images) < 1:
-            case_images = cls.objects.filter(categories__name__contains = search_term.capitalize())
-            return case_images
-        else:
-            return images
-    @classmethod
     
+    @classmethod
+   def get_image_by_id(cls,id):
+        image = cls.objects.get(id = id)
+        return image
+ 
     @classmethod
     def filter_by_location(cls,search_term):
         location = Location.objects.get(name = search_term)
