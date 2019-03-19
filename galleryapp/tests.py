37f757f5e58798857a04fcc6ee5cae29efe1f319
mukasine marie claire <mclaremukasine@gmail.com>
2019-03-19 10:35:29 +0200

    adding fonction of test-save-image  in tests.py

--- galleryapp/tests.py
+++ galleryapp/tests.py
@@ -74,3 +74,29 @@ class ImageTestClass(TestCase):
         self.searched_image.delete_image()
         self.assertTrue(len(Image.objects.all()) == 0)
 
+class CategoriesTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.category = categories(id = 1,name = 'test')
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,categories))
+
+    #Testing Save method
+    def test_save_method(self):
+        self.category.save_category()
+        self.categories = categories.objects.all()
+        self.assertTrue(len(self.categories) > 0)
+
+    def test_update_method(self):
+        self.category.save_category()
+        self.category = categories.objects.filter(name = 'test').update(name = 'changed')
+        self.updated_category = categories.objects.get(name = 'changed')
+        self.assertEqual(self.updated_category.name,"changed")
+
+    def test_delete_method(self):
+        self.category.save_category()
+        self.category = categories.objects.get(id = 1)
+        self.category.delete_category()
+        self.assertTrue(len(categories.objects.all()) == 0)
