cf55a965285006f9d7600037226c94f1cdca050b
mukasine marie claire <mclaremukasine@gmail.com>
2019-03-19 11:00:10 +0200

    adding class fonctoin of search in views.py

--- galleryapp/views.py
+++ galleryapp/views.py
@@ -9,9 +9,9 @@ def main_gallery(request):
     locations = Location.objects.all()
     return render(request, 'index.html', {"images":images,"locations":locations})
 
+def location(request,location):
+    locations = Location.objects.all()
+    selected_location = Location.objects.get(id = location)
+    images = Image.objects.filter(location = selected_location.id)
+    return render(request, 'location.html', {"location":selected_location,"locations":locations,"images":images})
 
-def search(request):
-    if 'category' in request.GET and request.GET["category"]:
-        search_term = request.GET.get("category")
-        searched_images = Image.search_by_category(search_term)
-    return render(request,'search.html',{"images":searched_images,"category":search_term})
