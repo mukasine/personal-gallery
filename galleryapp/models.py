55bae18bb3ae884ffc4ff4212d58ba18de3f5994
mukasine marie claire <mclaremukasine@gmail.com>
2019-03-19 10:05:01 +0200

    adding class location in models.py

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class categories(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
 
 class Location(models.Model):
     name = models.CharField(max_length=30)
