e487b447326002c4e07923cd43fe07fd55598054
mukasine marie claire <mclaremukasine@gmail.com>
2019-03-19 10:44:30 +0200

    adding class locationtestclas in tests.py

--- galleryapp/tests.py
+++ galleryapp/tests.py
@@ -2,32 +2,53 @@ from django.test import TestCase
 from .models import Image, Location, categories
 
 # Create your tests here.
-class LocationTestClass(TestCase):
+
+
+class ImageTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.location = Location(id = 1,name = 'Gasabo')
+        self.location = Location(name = "Gasabo")
+        self.location.save_location()
+        self.image = Image(id = 1,title = "test",description = "test description",location = self.location, image_url = "path/to/image")
 
     #Testing instance
-  def test_instance(self):
-        self.assertTrue(isinstance(self.location,Location))  
-    #testing the save method
-    def save_save_method(self):
-        self.james.save_location()
-        self.Gasabo.save_location()
-        self.assertTrue(len(locations) > 0)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
 
-    def test_update_method(self):
-        self.location.save_location()
-        self.location = Location.objects.filter(name = 'Gasabo').update(name = 'Kigali')
-        self.updated_location = Location.objects.get(name = 'Kigali')
-        self.assertEqual(self.updated_location.name,"Kigali")
+    def test_save_image(self):
+        self.image.save_image()
+        self.images = Image.objects.all()
+        self.assertTrue(len(self.images) > 0)
 
-    def test_delete_method(self):
-        self.location.save_location()
-        self.location = Location.objects.get(id = 1)
-        self.location.delete_location()
-        self.assertTrue(len(Location.objects.all()) == 0)
+    def test_get_image_by_id(self):
+        self.image.save_image()
+        self.image = Image.get_image_by_id(1)
+        self.assertTrue(isinstance(self.image,Image))
 
+    def test_update_image(self):
+        self.image.save_image()
+        self.image = Image.objects.filter(id = 1).update(image_url = "new/path/to/image")
+        self.updated_image = Image.get_image_by_id(1)
+        self.assertEqual(self.updated_image.image_url,"new/path/to/image")
+
+    def test_search_by_category(self):
+        self.image.save_image()
+        self.category = categories(name = "test")
+        self.category.save_category()
+        self.image = Image.get_image_by_id(1).categories.add(self.category)
+        self.searched_images = Image.search_by_category('test')
+        self.assertTrue(len(self.searched_images) > 0)
+
+    def test_filter_by_location(self):
+        self.image.save_image()
+        self.searched_images = Image.filter_by_location('Gasabo')
+        self.assertTrue(len(self.searched_images) > 0)
+
+    def test_delete_image(self):
+        self.image.save_image()
+        self.searched_image = Image.get_image_by_id(1)
+        self.searched_image.delete_image()
+        self.assertTrue(len(Image.objects.all()) == 0)
 
 class CategoriesTestClass(TestCase):
     # Set up method
