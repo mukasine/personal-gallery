1f037cb7f22341558019ff383af36288f10f2d55
mukasine marie claire <mclaremukasine@gmail.com>
2019-03-19 10:03:08 +0200

    adding class categories in models.py

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class categories(models.Model):
-    name = models.CharField(max_length=30)
-
-    def save_category(self):
-        self.save()
-
-    def delete_category(self):
-        self.delete()
-
-    def __str__(self):
-        return self.name
 
 class Location(models.Model):
     name = models.CharField(max_length=30)
